2997944efca35c1fefa16b0bb686a61714a82696
Francisco Fuentes <ffuentes@emergya.com>
2020-05-08 23:31:20 +0200

    Añadida creación de datos básica con faker para cargar partida

--- tests/test_gtkgui.py
+++ tests/test_gtkgui.py
@@ -1,12 +1,12 @@
+import time
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-
 from unittest import TestCase, main
+from faker import Faker
 
 from gui.gtkgui import run_gui
-
-import time
+from core.models import *
 
 # http://unpythonic.blogspot.com/2007/03/unit-testing-pygtk.html
 def refresh_gui(delay=0):
@@ -14,6 +14,41 @@ def refresh_gui(delay=0):
         Gtk.main_iteration_do(False)
 
     time.sleep(delay)
+
+
+class DatabaseCreator:
+    __instance = None
+
+    def __init__(self, con):
+        # añadir conexión con la que interaccionaremos con la base de datos
+        self.con = con
+
+        fake = Faker()
+        default_values = {
+            'partida':{
+                'nombre': fake.catch_phrase(),
+                'descripcion': fake.paragraph(),
+            }
+        }
+
+        self.default_values = default_values
+
+    @classmethod
+    def get_instance(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = cls(*args, **kwargs)
+
+        return cls.__instance
+
+    def fill_full_db(self):
+        self.run_crear_partida()
+
+    def crear_partida(self):
+        return self.con.crear_partida(
+            nombre=self.default_values['partida']['nombre'],
+            descripcion=self.default_values['partida']['descripcion'],
+        )
+
 
 class BaseTestGtkGui(TestCase):
     def creat_partida(self, bt_crear_partida, txt_crear_partida):
@@ -25,11 +60,15 @@ class BaseTestGtkGui(TestCase):
         return txt_partida
 
     def setUp(self):
+        init_db(test=True)
         self.gui = run_gui(test=True)
         self.con = self.gui.get_controller()
 
     def tearDown(self):
+        self.con.drop_db()
+        self.con.close_db()
         self.gui.exit()
+
 
 class CrearPartidaTest(BaseTestGtkGui):
     def test_crear_partida(self):
@@ -57,6 +96,7 @@ class CrearPartidaTest(BaseTestGtkGui):
         id_partida = cselpartida.get_model()[0][0]
         self.assertEqual(txt_partida, nombre_partida)
         self.assertEqual(id_partida, partidas[0].id)
+
 
 class BorrarPartidaTest(BaseTestGtkGui):
     def test_borrar_partidad(self):
@@ -86,12 +126,12 @@ class BorrarPartidaTest(BaseTestGtkGui):
 
 
 class CargarPartidaTest(BaseTestGtkGui):
-    def test_cargar_partida(self):
-        bt_crear_partida = self.gui.builder.get_object("button-new-partida")
-        bt_load_partida = self.gui.builder.get_object("button-load-partida")
-        txt_crear_partida = self.gui.builder.get_object("entry-partida")
-        txt_partida = self.creat_partida(bt_crear_partida, txt_crear_partida)
+    def setUp(self):
+        super().setUp()
+        self.db_creator = DatabaseCreator.get_instance(con=self.con)
+        self.db_creator.crear_partida()
 
+    def test_cargar_partida(self):
         # seleccionar fila
         cselpartida = self.gui.get_object("combo-partida")
         cselpartida.popup() # quizas no sea necesario ahora
@@ -99,5 +139,8 @@ class CargarPartidaTest(BaseTestGtkGui):
         cselpartida.popdown() # quizás no sea necesario
 
         # click en cargar
+        bt_load_partida = self.gui.builder.get_object("button-load-partida")
         bt_load_partida.clicked()
         refresh_gui()
+
+        # comprobar pestañas cargadas
